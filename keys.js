03c32ccdd884393698171c4d3ff3d9167eda77f5
Esperanza Freeman <el.freeman1@yahoo.com>
2017-11-15 17:28:21 -0500

    first

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '	EOXIQDxP1dJCYHuj7Aube58s7',
+  consumer_secret: 'DDAFmJFIKMEsvsMqkobKgOFam5roeVnDOOGzlPheW6GzhGqGiK',
+  access_token_key: '930629880831791104-uFZ1tfEUtlUjpX7Hbl85S69AXhwj3h1',
+  access_token_secret: 'gcTeh3VsvikMne6rLTAZs1Jq2p74j1Z3qvRWfpPNMyyqs',
+}
+
+module.exports = twitterKeys;
